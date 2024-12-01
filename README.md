a020aef09299b16c5b903c6008e337b6493e84d8
victoorreis <152341374+victoorreis@users.noreply.github.com>
2024-12-01 11:36:03 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# PROVA-2-BIMESTRES
-prova segundo bimestre
+# Online Store Dashboard
+
+## Descrição
+Este projeto é um painel de controle para gerenciar produtos e carrinhos de uma loja online. Ele oferece funcionalidades de CRUD para produtos e carrinhos, além de filtros e ordenação para facilitar a visualização dos dados.
+
+## Tecnologias Utilizadas
+- Vue.js
+- Vue Router
+- Vuex
+- Axios
+- ESLint
+
+## Funcionalidades Principais
+- **Gestão de Usuários**: Permite ~~adicionar~~, editar, listar e excluir usuários.
+- **Gestão de Produtos**: Permite ~~adicionar~~, editar, listar e excluir produtos.
+- **Gestão de Carrinhos**: Permite ~~adicionar~~, visualizar, listar e excluir carrinhos.
+- **Filtros e Ordenação**: Opções para filtrar por categoria e ordenar por preço ou classificação.
+- **Validação de Exclusão**: Impede a exclusão de produtos que estejam associados a carrinhos.
+
+## Instruções de Execução
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/tiagogregc/ProvaFakestore
+   ```
+2. Abra a pasta do repositório com o vs code
+
+3. Abra o terminal, atalho ctrl + j
+
+4. Instale as dependências com o comando
+    ```bash
+    npm i
+    ```
+
+5. Inicie o projeto com o comando
+    ```bash
+    npm run serve
+    ```
+
+6. Acesse a url abaixo para poder testar o projeto
+    ```bash
+    http://localhost:8080/
+    ```
+
---
+++ app.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Store Dashboard</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Overview</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/products">Produtos</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/carts">Carrinho</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/users">Usuários</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
